f6e6a19785f5a3488b611ce04eec58a44b291850
Gonza <gonzagramaglia@gmail.com>
2023-05-27 08:51:53 -0300

    Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,34 @@
 import { Inter } from 'next/font/google'
 import { FaGithub } from 'react-icons/fa';
-import logo from '../public/logo.png';
 import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='container mx-auto px-10 b-8'>
-      <Head>
-        <title>Blog</title>
-        <link rel='icon' href='/react-logo.png'/>
-      </Head>
-      <nav className='sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20 bg-{#050816}'>
-        <a 
-            href="https://www.gonza.uno/" 
-            target="_blank"
-        >
-            <img src={logo} alt="logo" className='' /> 
-        </a>
-        <a 
-            href="https://github.com/gonzagramaglia/blog" 
-            className='github-logo-container'
-            target="_blank"
-        >
-            <FaGithub size={23} style={{ color: '#a9a6c3' }} />
-        </a>
-    </nav>
-      <h1 className='text-[20px] mt-[200px]'>UNDER CONSTRUCTION</h1>
-    </div>
+    <>
+        <Head>
+          <title>Blog</title>
+          <link rel='icon' href='/react-logo.png'/>
+        </Head>
+        <nav className='sm:px-16 w-full h-[150px] flex items-center fixed top-0 z-20 bg-[#050816]'>
+          <a 
+              href="https://www.gonza.uno/" 
+              target="_blank"
+
+          >
+              <img src='/logo.png' alt="logo" className='h-8 ml-8' /> 
+          </a>
+          <a 
+              href="https://github.com/gonzagramaglia/blog" 
+              className='ml-auto mr-5'
+              target="_blank"
+          >
+              <FaGithub size={31} style={{ color: '#a9a6c3' }} />
+          </a>
+        </nav>
+        <div className='container mx-auto px-10 b-8'>
+        </div>
+    </>
   )
 }
