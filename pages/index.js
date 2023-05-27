11fc3d40689b0f1ce243f8cebed0af13b962ed67
Gonza <gonzagramaglia@gmail.com>
2023-05-27 09:00:17 -0300

    Navbar

--- pages/index.js
+++ pages/index.js
@@ -11,20 +11,21 @@ export default function Home() {
           <title>Blog</title>
           <link rel='icon' href='/react-logo.png'/>
         </Head>
-        <nav className='sm:px-16 w-full h-[150px] flex items-center fixed top-0 z-20 bg-[#050816]'>
+        <nav className='sm:px-16 w-full h-[110px] flex items-center fixed top-0 z-20 bg-[#050816]'>
           <a 
               href="https://www.gonza.uno/" 
+              className='pl-[65px]'
               target="_blank"
 
           >
-              <img src='/logo.png' alt="logo" className='h-8 ml-8' /> 
+              <img src='/logo.png' alt="logo" className='h-[25px]' /> 
           </a>
           <a 
               href="https://github.com/gonzagramaglia/blog" 
-              className='ml-auto mr-5'
+              className='ml-auto mr-[65px]'
               target="_blank"
           >
-              <FaGithub size={31} style={{ color: '#a9a6c3' }} />
+              <FaGithub size={23} style={{ color: '#a9a6c3' }} />
           </a>
         </nav>
         <div className='container mx-auto px-10 b-8'>
