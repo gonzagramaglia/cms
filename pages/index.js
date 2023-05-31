99261231d25e2f7c8dbd1a6e9311bc2325c6feb4
Gonza <gonzagramaglia@gmail.com>
2023-05-31 16:16:32 -0300

    Project Structure

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
-import { Inter } from 'next/font/google'
-import { FaGithub } from 'react-icons/fa';
+  import { FaGithub } from 'react-icons/fa';
 import Head from 'next/head'
 
-const inter = Inter({ subsets: ['latin'] })
+const posts = [
+  {
+    title: 'TailwindCSS',
+    excerpt: 'Learn TailwindCSS'
+  },
+  {
+    title: 'Habits',
+    excerpt: 'How to build great Habits'
+  }
+];
 
 export default function Home() {
   return (
@@ -28,7 +36,23 @@ export default function Home() {
               <FaGithub size={23} style={{ color: '#a9a6c3' }} />
           </a>
         </nav>
-        <div className='container mx-auto px-10 b-8'>
+        <div className='container mx-auto px-10 mb-8 bg-gray-700 '>
+          <div className='grid grid-cols-1 lg:grid-cols-12 gap-12 '>
+            <div className='lg:col-span-8 col-span-1'>
+              {posts.map( (post, index) => (
+                  <div>
+                    <h2 className='font-bold '>{post.title}</h2>
+                    <h6>{post.excerpt}</h6>
+                  </div>
+                ))}
+            </div>
+            <div className='lg:col-span-4 col-span-1 ' >
+              <div className='lg:sticky relative top-8'>
+
+              </div>
+
+            </div>
+          </div>
         </div>
     </>
   )
