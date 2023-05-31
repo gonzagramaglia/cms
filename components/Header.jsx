e41db375c426e541a1071a22b25048e312335d9b
Gonza <gonzagramaglia@gmail.com>
2023-05-31 17:42:19 -0300

    Branding

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { FaGithub } from 'react-icons/fa';
 
 import Link from 'next/link';
 
@@ -6,28 +7,47 @@ const categories = [ { name: "Web Development", slug: "webdev" }, { name: "Healt
 
 const Header = () => {
   return (
-    <div className='container mx-auto px-10 mb-8' >
-        <div className='border-b w-full inline-block border-blue-400 py-8' >
-            <div className='md:float-left block'>
-                <Link href="/">
-                    <span className='cursor-pointer font-bold text-4xl text-white' >
-                        Blog
-                    </span>
-                </Link>
-            </div>
-            <div className='hidden md:float-left md:contents'>
-                {
-                    categories.map( (category) => (
-                        <Link key={category.slug} href={`category/${category.slug}`}>
-                            <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer' >
-                                {category.name}
-                            </span>
-                        </Link>
-                    ) )
-                }
+    <>
+        <nav className='w-full h-[80px] md:h-[110px] flex items-center bg-[#050816]'>
+          <a 
+              href="https://www.gonza.gr/" 
+              className='ml-[20px] md:ml-[65px]'
+              target="_blank"
+
+          >
+              <img src='https://i.postimg.cc/9z5zvr3M/logo.png' alt="logo" className='h-[20px] md:h-[25px]' /> 
+          </a>
+          <a 
+              href="https://github.com/gonzagramaglia/blog" 
+              className='ml-auto mr-[20px] md:mr-[65px]'
+              target="_blank"
+          >
+              <FaGithub size={23} style={{ color: '#a9a6c3' }} />
+          </a>
+        </nav>
+        <div className='container mx-auto px-10 mb-8' >
+            <div className='border-b w-full inline-block border-blue-400 py-8' >
+                <div className='md:float-left block'>
+                    <Link href="/">
+                        <span className='cursor-pointer font-bold text-4xl text-white' >
+                            Blog
+                        </span>
+                    </Link>
+                </div>
+                <div className='hidden md:float-left md:contents'>
+                    {
+                        categories.map( (category) => (
+                            <Link key={category.slug} href={`category/${category.slug}`}>
+                                <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer' >
+                                    {category.name}
+                                </span>
+                            </Link>
+                        ) )
+                    }
+                </div>
             </div>
         </div>
-    </div>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -21,23 +21,6 @@ export default function Home() {
           <title>Blog</title>
           <link rel='icon' href='https://i.postimg.cc/nzNvhVb8/react-logo.png'/>
         </Head>
-        <nav className='w-full h-[80px] md:h-[110px] flex items-center bg-[#050816]'>
-          <a 
-              href="https://www.gonza.gr/" 
-              className='ml-[20px] md:ml-[65px]'
-              target="_blank"
-
-          >
-              <img src='https://i.postimg.cc/9z5zvr3M/logo.png' alt="logo" className='h-[20px] md:h-[25px]' /> 
-          </a>
-          <a 
-              href="https://github.com/gonzagramaglia/blog" 
-              className='ml-auto mr-[20px] md:mr-[65px]'
-              target="_blank"
-          >
-              <FaGithub size={23} style={{ color: '#a9a6c3' }} />
-          </a>
-        </nav>
         <div className='container mx-auto px-10 mb-8 bg-gray-700 '>
           <div className='grid grid-cols-1 lg:grid-cols-12 gap-12 '>
             <div className='lg:col-span-8 col-span-1'>
