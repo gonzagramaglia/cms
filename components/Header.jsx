a694edcb455b645859abbf483beb9acd80368ce3
Gonza <gonzagramaglia@gmail.com>
2023-06-01 00:48:47 -0300

    Categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,53 +1,61 @@
-import React, { useContext } from 'react'
+import React, { useState, useEffect } from 'react';
 import { FaGithub } from 'react-icons/fa';
 
 import Link from 'next/link';
 
-const categories = [ { name: "Web Development", slug: "webdev" }, { name: "Healthy Lifestyle", slug: "healthier" } ]
+import { getCategories } from '@/services';
+
 
 const Header = () => {
-  return (
-    <>
-        <nav className='w-full h-[80px] md:h-[110px] flex items-center bg-[#050816]'>
-          <a 
-              href="https://www.gonza.gr/" 
-              className='ml-[20px] md:ml-[65px]'
-              target="_blank"
+    const [categories, setCategories] = useState([]);
 
-          >
-              <img src='https://i.postimg.cc/9z5zvr3M/logo.png' alt="logo" className='h-[20px] md:h-[25px]' /> 
-          </a>
-          <a 
-              href="https://github.com/gonzagramaglia/blog" 
-              className='ml-auto mr-[20px] md:mr-[65px]'
-              target="_blank"
-          >
-              <FaGithub size={23} style={{ color: '#a9a6c3' }} />
-          </a>
-        </nav>
-        <div className='container mx-auto px-10 mb-8' >
-            <div className='border-b w-full inline-block border-blue-400 py-8' >
-                <div className='md:float-left block'>
-                    <Link href="/">
-                        <span className='cursor-pointer font-bold text-4xl text-white' >
-                            Blog
-                        </span>
-                    </Link>
-                </div>
-                <div className='hidden md:float-left md:contents'>
-                    {
-                        categories.map( (category) => (
-                            <Link key={category.slug} href={`category/${category.slug}`}>
-                                <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer' >
-                                    {category.name}
-                                </span>
-                            </Link>
-                        ) )
-                    }
+    useEffect(() => {
+        getCategories()
+        .then( (newCategories) => setCategories(newCategories) )
+    }, [])
+
+    return (
+        <>
+            <nav className='w-full h-[80px] md:h-[110px] flex items-center bg-[#050816]'>
+            <a 
+                href="https://www.gonza.gr/" 
+                className='ml-[20px] md:ml-[65px]'
+                target="_blank"
+
+            >
+                <img src='https://i.postimg.cc/9z5zvr3M/logo.png' alt="logo" className='h-[20px] md:h-[25px]' /> 
+            </a>
+            <a 
+                href="https://github.com/gonzagramaglia/blog" 
+                className='ml-auto mr-[20px] md:mr-[65px]'
+                target="_blank"
+            >
+                <FaGithub size={23} style={{ color: '#a9a6c3' }} />
+            </a>
+            </nav>
+            <div className='container mx-auto px-10 mb-8' >
+                <div className='border-b w-full inline-block border-blue-400 py-8' >
+                    <div className='md:float-left block'>
+                        <Link href="/">
+                            <span className='cursor-pointer font-bold text-4xl text-white' >
+                                Blog
+                            </span>
+                        </Link>
+                    </div>
+                    <div className='hidden md:float-left md:contents'>
+                        {
+                            categories.map( (category) => (
+                                <Link key={category.slug} href={`category/${category.slug}`}>
+                                    <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer' >
+                                        {category.name}
+                                    </span>
+                                </Link>
+                            ) )
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
+        </>
   )
 }
 
