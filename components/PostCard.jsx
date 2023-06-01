d9d70da89708014cbf0569d23dd1b076cb9119f9
Gonza <gonzagramaglia@gmail.com>
2023-05-31 22:15:12 -0300

    Post Display

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
 const PostCard = ({ post  }) => {
   return (
-    <div>
-        <h2 className='font-bold '>{post.title}</h2>
-        <h6>{post.excerpt}</h6>
+    <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8' >
+        <div className='relative overflow-hidden shadow-md pb-80 mb-6' >
+            <img 
+                className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
+                src={post.featuredImage.url}
+                alt={post.title}
+            />
+        </div>
+        <h1 className='transition duration-500 text-center mb-8 cursor-pointer hover:text-emerald-500 text-3xl font-semibold'>
+            <Link href={`/post/${post.slug}`} >
+                {post.title}
+            </Link>
+        </h1>
+        <div className='block lg:flex text-center items-center justify-center mb-8 w-full' >
+            <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8' >
+                {post.author.photo && (
+                    <img 
+                        alt={post.author.name}
+                        height='30px'
+                        width='30px'
+                        className='align-middle rounded-full'
+                        src={post.author.photo.url}
+                    />
+                )}
+                <p className='inline align-middle text-gray-700 ml-2 text-lg' >{post.author.name}</p>
+            </div>
+            <div className='font-medium text-gray-700' >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-pink-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
+                <span className="align-middle">{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+            </div>
+        </div>
+        <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-12 mb-8'>
+            {post.excerpt}
+        </p>
+        <div className='text-center'>
+            <Link href={`post/${post.slug}`} >
+                <span className='transition duration-300 transform hover:-translate-y-0.5 hover:shadow-lg hover:text-emerald-800 inline-block bg-emerald-500 text-white text-lg font-medium rounded-full px-8 py-3 cursor-pointer' >
+                    Continue Reading
+                </span>
+            </Link>
+        </div>
     </div>
   )
 }
