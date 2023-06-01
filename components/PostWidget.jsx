16d323049876f8ff2512ee7a372308c4b39ee365
Gonza <gonzagramaglia@gmail.com>
2023-06-01 00:20:37 -0300

    Post Widget

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -18,7 +18,34 @@ function PostWidget({ categories, slug }) {
     }, [slug] )
 
     return (
-        <div>PostWidget</div>
+        <div className='bg-white shadow-lg rounded-lg p-8 mb-8' >
+            <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+                { slug ? 'Related Posts' : 'Recent Posts' }
+            </h3>
+            {
+                relatedPosts.map( (post) => (
+                    <div key={post.title} className='flex items-center w-full mb-4' >
+                        <div className='w-16 flex-none' >
+                            <img 
+                                alt={post.title}
+                                height="60px"
+                                width="60px"
+                                className='align-middle rounded-full'
+                                src={post.featuredImage.url}
+                            />
+                        </div>
+                        <div className='flex-grow ml-4' >
+                            <Link href={`/post/${post.slug}`} key={post.title} className='text-md' >
+                                { post.title.substring(0, post.title.indexOf(":")) }
+                            </Link>
+                            <p className='text-gray-500 font-xs' >
+                                {moment(post.createdAt).format('MMM DD, YYYY')}
+                            </p>
+                        </div>
+                    </div>
+                 ) )
+            }
+        </div>
     )
 }
 
