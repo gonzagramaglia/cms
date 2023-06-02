c839d1fec6812de1cc25d406c1b3bafd02212902
Gonza <gonzagramaglia@gmail.com>
2023-06-02 12:19:49 -0300

    Post Detail

--- services/index.js
+++ services/index.js
@@ -39,6 +39,42 @@ export const getPosts  = async () => {
 }
 
 
+export const getPostDetails  = async (slug) => {
+    const query = gql`
+        query GetPostDetails($slug: String!) {
+            post(where: {slug: $slug}){
+                author {
+                    bio
+                    name
+                    id
+                    photo {
+                        url
+                    }
+                }
+                createdAt
+                slug
+                title
+                excerpt
+                featuredImage {
+                    url
+                }
+                categories {
+                    name
+                    slug
+                }
+                content {
+                    raw
+                }
+            }               
+        }
+    `
+
+    const result = await request(graphqlAPI, query, { slug } );
+
+    return result.post;
+} 
+
+
 
 export const getRecentPosts = async () => {
     const query = gql`
@@ -63,11 +99,11 @@ export const getRecentPosts = async () => {
 
 
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
         query GetPostDetails($slug: String!, $categories: [String!]) {
             posts(
-                where: { slug_not: $slug, AND { categories_some: { slug_in: $categories } } }
+                where: { slug_not: $slug, AND: { categories_some: { slug_in: $categories } } }
                 last: 3
             ) {
                 title
@@ -80,7 +116,7 @@ export const getSimilarPosts = async () => {
         }
     `
 
-    const result = await request(graphqlAPI, query);
+    const result = await request(graphqlAPI, query, { categories, slug } );
 
     return result.posts;
 }
