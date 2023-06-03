09855ed8ab6fe8e1f505caa09e5f6d86076ec94a
Gonza <gonzagramaglia@gmail.com>
2023-06-02 23:58:53 -0300

    UI Upgrade

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -18,7 +18,7 @@ const PostDetails = ({post}) => {
                     <Comments slug={post.slug} />
                 </div>
                 <div className='col-span-1 lg:col-span-4'>
-                    <div className='relative lg:sticky top-8'>
+                    <div className='relative lg:sticky top-32'>
                         <PostWidget slug={post.slug} categories={post.categories.map( (category) => category.slug )} />
                         <Categories />
                     </div>
