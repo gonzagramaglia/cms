a39423ad59e54507e0070d64472c50029cdd806b
Gonza <gonzagramaglia@gmail.com>
2023-06-03 00:04:52 -0300

    .env removed

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -93,10 +93,10 @@ const PostDetail = ({post}) => {
                     </div>
                 </div>
                 <h1 className='mb-8 text-3xl font-semibold text-left'>{post.title}</h1>
-                {post.content.raw.children.map((typeObj, index) => {
-                  const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
+                {post.content.raw.children.map( (typeObj, index) => {
+                    const children = typeObj.children.map( (item, itemIndex) => getContentFragment(itemIndex, item.text, item) )
 
-                  return getContentFragment(index, children, typeObj, typeObj.type);
+                    return getContentFragment(index, children, typeObj, typeObj.type)
                 })}
             </div>
         </div>
